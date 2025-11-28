5da678189bf980c8bb4f5cd5406ed5141af72771
ConCorde0 <1462913972@qq.com>
2025-11-28 20:53:53 +0800

    :sparkles: feat:完成基本输入控制

--- Assets/a_Scripts/Car/CarController.cs
+++ Assets/a_Scripts/Car/CarController.cs
@@ -1,16 +1,22 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using a_Scripts;
 using UnityEngine;
 
 public class CarController : MonoBehaviour
 {
+
+    private CarInputSystem _input;
+    
     public Transform[] wheelMeshes;
     public WheelCollider[] wheelColliders;
 
     public int rotateSpeed;
     public int rotationAngle;
     public int wheelRotateSpeed;
+    
+    private int targetRotation;
 
     private void LateUpdate()
     {
@@ -22,5 +28,37 @@ public class CarController : MonoBehaviour
             wheelMeshes[i].position = pos;
             wheelMeshes[i].Rotate(Vector3.right * (Time.deltaTime * wheelRotateSpeed));
         }
+        
+        if(_input.Clicked || _input.Horizontal != 0)
+        {
+            UpdateTargetRotation();
+        }
+        else if(targetRotation != 0)
+        {
+            targetRotation = 0;
+        }
+        
+        Vector3 rotation = new Vector3(transform.localEulerAngles.x, targetRotation, transform.localEulerAngles.z);
+        transform.rotation = Quaternion.RotateTowards(transform.rotation, Quaternion.Euler(rotation), rotateSpeed * Time.deltaTime);
+        
     }
+
+    private void UpdateTargetRotation()
+    {
+        if(_input.Horizontal == 0){
+            if(Input.mousePosition.x > Screen.width * 0.5f)
+            {
+                targetRotation = rotationAngle;
+            }
+            else
+            {
+                targetRotation = -rotationAngle;
+            }
+        }
+        else{
+            targetRotation = (int)(rotationAngle * _input.Horizontal);
+        }
+    }
+    
+    
 }
---
+++ Assets/a_Scripts/Car/CarInputSystem.cs
@@ -0,0 +1,36 @@
+using System;
+using UnityEngine;
+
+namespace a_Scripts
+{
+    public class CarInputSystem : MonoBehaviour
+    {
+        private PlayerInput _input;
+        
+        public float Horizontal { get; private set; }
+        public bool Clicked { get; private set; }
+        public Vector2 MousePosition { get; private set; }
+        
+        private void Awake()
+        {
+            _input = new PlayerInput();
+        }
+
+        private void OnEnable()
+        {
+            _input.Enable();
+            _input.GamePlay.Move.performed += ctx => Horizontal = ctx.ReadValue<float>();
+            _input.GamePlay.Move.canceled += ctx => Horizontal = 0;
+            
+            _input.GamePlay.Click.performed += ctx => Clicked = true;
+            _input.GamePlay.Click.canceled += ctx => Clicked = false;
+
+            _input.GamePlay.Mouse.performed += ctx => MousePosition = ctx.ReadValue<Vector2>();
+        }
+
+        private void OnDisable()
+        {
+            _input.Disable();
+        }
+    }
+}
